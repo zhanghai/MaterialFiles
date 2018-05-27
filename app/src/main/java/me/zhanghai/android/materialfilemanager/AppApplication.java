c5c57b328676114718923be7e2df82bdf66555cc
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-05-26 20:08:28 -0700

    [Feature] Add some dependencies.

---
+++ app/src/main/java/me/zhanghai/android/materialfilemanager/AppApplication.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.materialfilemanager;
+
+import android.app.Application;
+
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+public class AppApplication extends Application {
+
+    private static AppApplication sInstance;
+
+    public AppApplication() {
+        sInstance = this;
+    }
+
+    public static AppApplication getInstance() {
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // TODO
+        //NightModeHelper.setup(this);
+
+        AndroidThreeTen.init(this);
+        //FabricUtils.init(this);
+    }
+}
