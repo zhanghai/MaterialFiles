b6afb26402522f9a789ce5244b03105dd3237586
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-05-31 22:45:28 +0800

    [Feature] Remove FileObserver.

--- app/src/main/java/me/zhanghai/android/materialfilemanager/filelist/FileLiveData.java
+++ app/src/main/java/me/zhanghai/android/materialfilemanager/filelist/FileLiveData.java
@@ -18,38 +18,14 @@ import me.zhanghai.android.materialfilemanager.filesystem.LocalFile;
 public class FileLiveData extends LiveData<File> {
 
     private Uri mPath;
-    private FileObserver mObserver;
-    private boolean mChanged;
 
     public FileLiveData(Uri path) {
         mPath = path;
-        // TODO: Is file Uri?
-        mObserver = new FileObserver(mPath.getPath(), FileObserver.MODIFY | FileObserver.ATTRIB
-                | FileObserver.MOVED_FROM | FileObserver.MOVED_TO | FileObserver.CREATE
-                | FileObserver.DELETE | FileObserver.DELETE_SELF | FileObserver.MOVE_SELF) {
-            @Override
-            public void onEvent(int event, @Nullable String path) {
-                if (hasActiveObservers()) {
-                    loadData();
-                } else {
-                    mChanged = true;
-                }
-            }
-        };
         loadData();
-    }
-
-    @Override
-    protected void onActive() {
-        if (mChanged) {
-            loadData();
-        }
-        mObserver.startWatching();
     }
 
     @SuppressLint("StaticFieldLeak")
     private void loadData() {
-        mChanged = false;
         new AsyncTask<Void, Void, File>() {
             @Override
             protected File doInBackground(Void... strings) {
