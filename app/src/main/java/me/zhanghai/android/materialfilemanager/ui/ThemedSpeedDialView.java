c642db0d13a2671fed4a7d06f7fb8b5c383bac24
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-11-06 20:16:04 -0800

    [Fix] Fix ThemedSpeedDialView restoring state; Fixes #58.

--- app/src/main/java/me/zhanghai/android/materialfilemanager/ui/ThemedSpeedDialView.java
+++ app/src/main/java/me/zhanghai/android/materialfilemanager/ui/ThemedSpeedDialView.java
@@ -6,27 +6,16 @@
 package me.zhanghai.android.materialfilemanager.ui;
 
 import android.content.Context;
-import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.leinardi.android.speeddial.SpeedDialActionItem;
 import com.leinardi.android.speeddial.SpeedDialView;
 
-import java.util.ArrayList;
-
-import androidx.annotation.MenuRes;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.PopupMenu;
 import me.zhanghai.android.materialfilemanager.R;
 import me.zhanghai.android.materialfilemanager.util.ViewUtils;
 
 public class ThemedSpeedDialView extends SpeedDialView {
-
-    private boolean mSavingInstanceState;
 
     public ThemedSpeedDialView(Context context) {
         super(context);
@@ -41,45 +30,23 @@ public class ThemedSpeedDialView extends SpeedDialView {
     }
 
     @Override
-    public void inflate(@MenuRes int menuRes) {
-        clearActionItems();
+    public void addActionItem(SpeedDialActionItem actionItem, int position, boolean animate) {
         Context context = getContext();
-        PopupMenu popupMenu = new PopupMenu(context, new View(context));
-        popupMenu.inflate(menuRes);
-        Menu menu = popupMenu.getMenu();
-        int floatingBackgroundColor = ViewUtils.getColorFromAttrRes(R.attr.colorBackgroundFloating,
-                0, context);
-        int secondaryTextColor = ViewUtils.getColorFromAttrRes(android.R.attr.textColorSecondary, 0,
+        int labelColor = ViewUtils.getColorFromAttrRes(android.R.attr.textColorSecondary, 0,
                 context);
-        for (int i = 0; i < menu.size(); i++) {
-            MenuItem menuItem = menu.getItem(i);
-            SpeedDialActionItem actionItem = new SpeedDialActionItem.Builder(menuItem.getItemId(),
-                    menuItem.getIcon())
-                    .setLabel(menuItem.getTitle() != null ? menuItem.getTitle().toString() : null)
-                    .setLabelBackgroundColor(floatingBackgroundColor)
-                    .setLabelColor(secondaryTextColor)
-                    .create();
-            addActionItem(actionItem);
-        }
-    }
-
-    // HACK: Don't save the action items so that they don't overwrite our new themed items when
-    // restoring saved state.
-    @Nullable
-    @Override
-    protected Parcelable onSaveInstanceState() {
-        mSavingInstanceState = true;
-        Parcelable savedState = super.onSaveInstanceState();
-        mSavingInstanceState = false;
-        return savedState;
-    }
-
-    @NonNull
-    @Override
-    public ArrayList<SpeedDialActionItem> getActionItems() {
-        if (mSavingInstanceState) {
-            return new ArrayList<>();
-        }
-        return super.getActionItems();
+        int labelBackgroundColor = ViewUtils.getColorFromAttrRes(R.attr.colorBackgroundFloating, 0,
+                context);
+        actionItem = new SpeedDialActionItem.Builder(actionItem.getId(),
+                // Should not be a resource, pass null to fail fast.
+                actionItem.getFabImageDrawable(null))
+                .setLabel(actionItem.getLabel())
+                .setFabImageTintColor(actionItem.getFabImageTintColor())
+                .setFabBackgroundColor(actionItem.getFabBackgroundColor())
+                .setLabelColor(labelColor)
+                .setLabelBackgroundColor(labelBackgroundColor)
+                .setLabelClickable(actionItem.isLabelClickable())
+                .setTheme(actionItem.getTheme())
+                .create();
+        super.addActionItem(actionItem, position, animate);
     }
 }
