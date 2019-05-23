0118679aa9628c269eb53287c7b794a65065c1ab
Zhang Hai <dreaming.in.code.zh@gmail.com>
2019-05-23 02:24:06 -0700

    [Feature] Update icon.

---
+++ art/copy-to-res.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+for dpi in {m,h,xh,xxh,xxxh}dpi; do
+    cp "launcher_icon-${dpi}.png" "../app/src/main/res/mipmap-${dpi}/launcher_icon.png"
+    cp "launcher_icon_foreground-${dpi}.png" "../app/src/main/res/mipmap-${dpi}/launcher_icon_foreground.png"
+done
+cp launcher_icon-web.png ../app/src/main/launcher_icon-web.png
