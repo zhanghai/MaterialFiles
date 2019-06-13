d921e7fdb1a3b60f1f8cba72ce4b78c1de60d826
Hai Zhang <dreaming.in.code.zh@gmail.com>
2019-06-13 14:29:30 -0700

    [Fix] Don't try to open text file larger than 1MB to avoid ANR.

--- app/src/main/java/me/zhanghai/android/files/viewer/FileContentLiveData.java
+++ app/src/main/java/me/zhanghai/android/files/viewer/FileContentLiveData.java
@@ -8,14 +8,19 @@ package me.zhanghai.android.files.viewer;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 
+import java.io.IOException;
+
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.WorkerThread;
 import androidx.lifecycle.LiveData;
+import java8.nio.file.Files;
 import java8.nio.file.Path;
 import me.zhanghai.android.files.provider.common.MoreFiles;
 
 public class FileContentLiveData extends LiveData<FileContentData> {
+
+    private static final long MAX_SIZE = 1024 * 1024;
 
     @NonNull
     private final Path mPath;
@@ -34,6 +39,9 @@ public class FileContentLiveData extends LiveData<FileContentData> {
             @WorkerThread
             protected FileContentData doInBackground(Void... parameters) {
                 try {
+                    if (Files.size(mPath) > MAX_SIZE) {
+                        throw new IOException("File is too large");
+                    }
                     byte[] content = MoreFiles.readAllBytes(mPath);
                     return FileContentData.ofSuccess(mPath, content);
                 } catch (Exception e) {
