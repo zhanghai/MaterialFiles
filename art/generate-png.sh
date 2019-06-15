0969f67b9be5c27b937a511e75fcbc08cd65ae8a
Hai Zhang <dreaming.in.code.zh@gmail.com>
2019-06-15 02:41:01 -0700

    [Feature] Add outer shadow for pre-O launcher icon.

--- art/generate-png.sh
+++ art/generate-png.sh
@@ -5,10 +5,14 @@ inkscape -e launcher_icon-hdpi.png -a 15:15:93:93 -w 72 -h 72 launcher_icon.svg
 inkscape -e launcher_icon-xhdpi.png -a 15:15:93:93 -w 96 -h 96 launcher_icon.svg
 inkscape -e launcher_icon-xxhdpi.png -a 15:15:93:93 -w 144 -h 144 launcher_icon.svg
 inkscape -e launcher_icon-xxxhdpi.png -a 15:15:93:93 -w 192 -h 192 launcher_icon.svg
-inkscape -e launcher_icon-web.png -a 18:18:90:90 -w 512 -h 512 launcher_icon.svg
+
+cp launcher_icon.svg launcher_icon-web.svg
+inkscape --select=circle --verb=RemoveFilter --verb=FileSave --verb=FileQuit launcher_icon-web.svg
+inkscape -e launcher_icon-web.png -a 18:18:90:90 -w 512 -h 512 launcher_icon-web.svg
+rm launcher_icon-web.svg
 
 cp launcher_icon.svg launcher_icon_foreground.svg
-inkscape --select=circle-clip-group --verb=SelectionUnGroup --verb=EditDeselect --select=circle --select=circle-edge-top --select=circle-edge-bottom --verb=EditDelete --verb=FileSave --verb=FileQuit launcher_icon_foreground.svg
+inkscape --select=circle --select=circle-edge-top --select=circle-edge-bottom --verb=EditDelete --select=circle-clip-group --verb=SelectionUnGroup --verb=FileSave --verb=FileQuit launcher_icon_foreground.svg
 inkscape -e launcher_icon_foreground-mdpi.png -w 108 -h 108 launcher_icon_foreground.svg
 inkscape -e launcher_icon_foreground-hdpi.png -w 162 -h 162 launcher_icon_foreground.svg
 inkscape -e launcher_icon_foreground-xhdpi.png -w 216 -h 216 launcher_icon_foreground.svg
