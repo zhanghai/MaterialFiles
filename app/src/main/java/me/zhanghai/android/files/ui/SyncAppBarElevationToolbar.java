9785d5415bdeb4484863ac31089847072f9cb00d
Hai Zhang <dreaming.in.code.zh@gmail.com>
2019-09-07 02:38:54 -0700

    [Fix] Correct elevation overlay for top/bottom toolbars.

---
+++ app/src/main/java/me/zhanghai/android/files/ui/SyncAppBarElevationToolbar.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2019 Hai Zhang <dreaming.in.code.zh@gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.files.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.ViewParent;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.shape.MaterialShapeDrawable;
+import com.google.android.material.shape.MaterialShapeUtils;
+
+import androidx.annotation.AttrRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SyncAppBarElevationToolbar extends MaterialToolbar {
+
+    public SyncAppBarElevationToolbar(@NonNull Context context) {
+        super(context);
+    }
+
+    public SyncAppBarElevationToolbar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SyncAppBarElevationToolbar(@NonNull Context context, @Nullable AttributeSet attrs,
+                                      @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        AppBarLayout appBarLayout = null;
+        ViewParent parent = getParent();
+        while (parent != null) {
+            if (parent instanceof AppBarLayout) {
+                appBarLayout = (AppBarLayout) parent;
+                break;
+            }
+            parent = parent.getParent();
+        }
+        if (appBarLayout != null) {
+            Drawable appBarLayoutBackground = appBarLayout.getBackground();
+            if (!(appBarLayoutBackground instanceof SetElevationCallbackDrawable)
+                    && appBarLayoutBackground instanceof MaterialShapeDrawable) {
+                appBarLayoutBackground = new SetElevationCallbackDrawable(
+                        (MaterialShapeDrawable) appBarLayoutBackground,
+                        () -> MaterialShapeUtils.setParentAbsoluteElevation(this),
+                        getContext());
+                appBarLayout.setBackground(appBarLayoutBackground);
+            }
+        }
+    }
+
+    private static class SetElevationCallbackDrawable extends MaterialShapeDrawable {
+
+        @NonNull
+        private final Runnable mSetElevationCallback;
+
+        public SetElevationCallbackDrawable(@NonNull MaterialShapeDrawable drawable,
+                                            @NonNull Runnable setElevationCallback,
+                                            @NonNull Context context) {
+            mSetElevationCallback = setElevationCallback;
+            setFillColor(drawable.getFillColor());
+            initializeElevationOverlay(context);
+        }
+
+        @Override
+        public void setElevation(float elevation) {
+            super.setElevation(elevation);
+
+            mSetElevationCallback.run();
+        }
+    }
+}
