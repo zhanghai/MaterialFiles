6d8e738e564b4bfd49e21eb8c6b90a83f0f3e25a
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-10-04 23:08:48 -0700

    [Feature] Use public API to update task color.

--- app/src/main/java/me/zhanghai/android/files/compat/ActivityCompat.kt
+++ app/src/main/java/me/zhanghai/android/files/compat/ActivityCompat.kt
@@ -11,13 +11,8 @@ import android.graphics.Color
 import android.os.Build
 import androidx.annotation.StyleRes
 import androidx.core.app.ActivityCompat
-import androidx.core.graphics.ColorUtils
 import me.zhanghai.android.files.R
 import me.zhanghai.android.files.util.getColorByAttr
-import me.zhanghai.java.reflected.ReflectedField
-
-@RestrictedHiddenApi
-private val taskDescriptionField = ReflectedField(Activity::class.java, "mTaskDescription")
 
 fun Activity.recreateCompat() {
     ActivityCompat.recreate(this)
@@ -26,15 +21,10 @@ fun Activity.recreateCompat() {
 fun Activity.setThemeCompat(@StyleRes resid: Int) {
     setTheme(resid)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        val taskDescription = taskDescriptionField.getObject<TaskDescription>(this)
-        var appBarSurfaceColor = getColorByAttr(R.attr.colorAppBarSurface)
-        if (appBarSurfaceColor == 0 || taskDescription.primaryColor == appBarSurfaceColor) {
-            return
+        val appBarSurfaceColor = getColorByAttr(R.attr.colorAppBarSurface)
+        if (appBarSurfaceColor != 0 && Color.alpha(appBarSurfaceColor) == 0xFF) {
+            @Suppress("DEPRECATION")
+            setTaskDescription(TaskDescription(null, null, appBarSurfaceColor))
         }
-        if (Color.alpha(appBarSurfaceColor) != 0xFF) {
-            appBarSurfaceColor = ColorUtils.setAlphaComponent(appBarSurfaceColor, 0xFF)
-        }
-        taskDescription.setPrimaryColorCompat(appBarSurfaceColor)
-        setTaskDescription(taskDescription)
     }
 }
