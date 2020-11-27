532bd801372f76f678252612514f82ccf37a9866
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-11-27 01:54:52 -0800

    [Feature] Use rounded ripple for breadcrumb.

--- app/src/main/java/me/zhanghai/android/files/compat/ViewCompat.kt
+++ app/src/main/java/me/zhanghai/android/files/compat/ViewCompat.kt
@@ -5,9 +5,13 @@
 
 package me.zhanghai.android.files.compat
 
+import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.core.view.ViewCompat
+import me.zhanghai.android.foregroundcompat.ForegroundCompat
 
 @Suppress("UNCHECKED_CAST")
 fun <T : View> View.requireViewByIdCompat(@IdRes id: Int) : T =
@@ -22,3 +26,27 @@ var View.scrollIndicatorsCompat: Int
 fun View.setScrollIndicatorsCompat(indicators: Int, mask: Int) {
     ViewCompat.setScrollIndicators(this, indicators, mask)
 }
+
+var View.foregroundCompat: Drawable?
+    get() = ForegroundCompat.getForeground(this)
+    set(value) {
+        ForegroundCompat.setForeground(this, value)
+    }
+
+var View.foregroundGravityCompat: Int
+    get() = ForegroundCompat.getForegroundGravity(this)
+    set(value) {
+        ForegroundCompat.setForegroundGravity(this, value)
+    }
+
+var View.foregroundTintListCompat: ColorStateList?
+    get() = ForegroundCompat.getForegroundTintList(this)
+    set(value) {
+        ForegroundCompat.setForegroundTintList(this, value)
+    }
+
+var View.foregroundTintModeCompat: PorterDuff.Mode?
+    get() = ForegroundCompat.getForegroundTintMode(this)
+    set(value) {
+        ForegroundCompat.setForegroundTintMode(this, value)
+    }
