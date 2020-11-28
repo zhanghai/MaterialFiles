cc14df99807b2890b467836f2b8b996dd6ed4ca8
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-11-28 02:06:32 -0800

    [Feature] Hide privacy policy in F-Droid build.
    We don't integrate any analytics/crash reporting service in F-Droid
    build, so showing a privacy policy there is misleading.

--- app/src/main/java/me/zhanghai/android/files/about/AboutFragment.kt
+++ app/src/main/java/me/zhanghai/android/files/about/AboutFragment.kt
@@ -12,6 +12,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import me.zhanghai.android.files.databinding.AboutFragmentBinding
 import me.zhanghai.android.files.ui.LicensesDialogFragment
@@ -43,9 +44,12 @@ class AboutFragment : Fragment() {
         activity.setSupportActionBar(binding.toolbar)
         binding.gitHubLayout.setOnClickListener { startActivitySafe(GITHUB_URI.createViewIntent()) }
         binding.licensesLayout.setOnClickListener { LicensesDialogFragment.show(this) }
+//#ifdef NONFREE
+        binding.privacyPolicyLayout.isVisible = true
         binding.privacyPolicyLayout.setOnClickListener {
             startActivitySafe(PRIVACY_POLICY_URI.createViewIntent())
         }
+//#endif
         binding.authorNameLayout.setOnClickListener {
             startActivitySafe(AUTHOR_RESUME_URI.createViewIntent())
         }
