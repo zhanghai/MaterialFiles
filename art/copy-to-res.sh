bba075fee6f0397879854833da612e1e3258fe35
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-11-30 20:39:39 -0800

    [Fix] Fix play icon color; Update fastlane icons.

--- art/copy-to-res.sh
+++ art/copy-to-res.sh
@@ -5,7 +5,6 @@ for dpi in {m,h,xh,xxh,xxxh}dpi; do
     cp "launcher_icon-${dpi}.png" "../app/src/main/res/mipmap-${dpi}/launcher_icon.png"
     cp "launcher_icon_foreground-${dpi}.png" "../app/src/main/res/mipmap-${dpi}/launcher_icon_foreground.png"
 done
-cp launcher_icon-web.png ../app/src/main/launcher_icon-web.png
 
 for shortcut in directory downloads file ftp_server; do
     for dpi in {m,h,xh,xxh,xxxh}dpi; do
