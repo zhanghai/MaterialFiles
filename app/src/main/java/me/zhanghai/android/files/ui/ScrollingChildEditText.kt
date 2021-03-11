24248e00453e0db89f91f5222bf447ebe8e9ac1b
Hai Zhang <dreaming.in.code.zh@gmail.com>
2021-03-11 01:37:53 -0800

    [Feature] Add an EditText that don't bring cursor into view on every measure.
    Fixes: #309

---
+++ app/src/main/java/me/zhanghai/android/files/ui/ScrollingChildEditText.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Hai Zhang <dreaming.in.code.zh@gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.files.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class ScrollingChildEditText : AppCompatEditText {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        // onMeasure() calls registerForPreDraw() and onPreDraw() calls bringPointIntoView(), which
+        // results in unwanted scroll when IME is hidden.
+        viewTreeObserver.removeOnPreDrawListener(this)
+    }
+}
