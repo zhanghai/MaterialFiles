0f0d306e5db2e2afea257449c050936c5a60a5c0
Hai Zhang <dreaming.in.code.zh@gmail.com>
2021-03-24 03:22:27 -0700

    [Fix] Fix image viewer not zoomable.
    Coil's TransitionDrawable needs another parameter to report the
    intrinsic size correctly.
    
    Also always use fadeInUnsafe(true) because we are binding views before
    being attached to window, so it will never be laid out at that time.
    
    Fixes: #437

---
+++ app/src/main/java/me/zhanghai/android/files/coil/CoilExtensions.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Hai Zhang <dreaming.in.code.zh@gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.files.coil
+
+import coil.annotation.ExperimentalCoilApi
+import coil.request.ImageRequest
+import coil.transition.CrossfadeTransition
+
+@OptIn(ExperimentalCoilApi::class)
+fun ImageRequest.Builder.fadeIn(durationMillis: Int): ImageRequest.Builder =
+    apply {
+        placeholder(android.R.color.transparent)
+        transition(CrossfadeTransition(durationMillis, true))
+    }
