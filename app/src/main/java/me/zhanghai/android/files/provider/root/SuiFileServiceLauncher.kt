c6a35ec6f5a8219c929a5e6b9a2d7e136afcd9a8
Hai Zhang <dreaming.in.code.zh@gmail.com>
2022-02-10 01:05:08 -0800

    [Fix] Catch InterruptedException when using runBlocking().

--- app/src/main/java/me/zhanghai/android/files/provider/root/SuiFileServiceLauncher.kt
+++ app/src/main/java/me/zhanghai/android/files/provider/root/SuiFileServiceLauncher.kt
@@ -53,83 +53,93 @@ object SuiFileServiceLauncher {
                 throw RemoteFileSystemException("Sui isn't available")
             }
             if (Shizuku.checkSelfPermission() != PackageManager.PERMISSION_GRANTED) {
-                val granted = runBlocking<Boolean> {
-                    suspendCancellableCoroutine { continuation ->
-                        val listener = object : Shizuku.OnRequestPermissionResultListener {
-                            override fun onRequestPermissionResult(
-                                requestCode: Int,
-                                grantResult: Int
-                            ) {
-                                Shizuku.removeRequestPermissionResultListener(this)
-                                val granted = grantResult == PackageManager.PERMISSION_GRANTED
-                                continuation.resume(granted)
+                val granted = try {
+                    runBlocking<Boolean> {
+                        suspendCancellableCoroutine { continuation ->
+                            val listener = object : Shizuku.OnRequestPermissionResultListener {
+                                override fun onRequestPermissionResult(
+                                    requestCode: Int,
+                                    grantResult: Int
+                                ) {
+                                    Shizuku.removeRequestPermissionResultListener(this)
+                                    val granted = grantResult == PackageManager.PERMISSION_GRANTED
+                                    continuation.resume(granted)
+                                }
                             }
+                            Shizuku.addRequestPermissionResultListener(listener)
+                            continuation.invokeOnCancellation {
+                                Shizuku.removeRequestPermissionResultListener(listener)
+                            }
+                            Shizuku.requestPermission(listener.hashCode())
                         }
-                        Shizuku.addRequestPermissionResultListener(listener)
-                        continuation.invokeOnCancellation {
-                            Shizuku.removeRequestPermissionResultListener(listener)
-                        }
-                        Shizuku.requestPermission(listener.hashCode())
                     }
+                } catch (e: InterruptedException) {
+                    throw RemoteFileSystemException(e)
                 }
                 if (!granted) {
                     throw RemoteFileSystemException("Sui permission isn't granted")
                 }
             }
-            return runBlocking {
-                try {
-                    withTimeout(RootFileService.TIMEOUT_MILLIS) {
-                        suspendCancellableCoroutine { continuation ->
-                            val serviceArgs = Shizuku.UserServiceArgs(
-                                ComponentName(application, SuiFileServiceInterface::class.java)
-                            )
-                                .debuggable(BuildConfig.DEBUG)
-                                .daemon(false)
-                                .processNameSuffix("sui")
-                                .version(BuildConfig.VERSION_CODE)
-                            val connection = object : ServiceConnection {
-                                override fun onServiceConnected(
-                                    name: ComponentName,
-                                    service: IBinder
-                                ) {
-                                    val serviceInterface =
-                                        IRemoteFileService.Stub.asInterface(service)
-                                    continuation.resume(serviceInterface)
-                                }
+            return try {
+                runBlocking {
+                    try {
+                        withTimeout(RootFileService.TIMEOUT_MILLIS) {
+                            suspendCancellableCoroutine { continuation ->
+                                val serviceArgs = Shizuku.UserServiceArgs(
+                                    ComponentName(application, SuiFileServiceInterface::class.java)
+                                )
+                                    .debuggable(BuildConfig.DEBUG)
+                                    .daemon(false)
+                                    .processNameSuffix("sui")
+                                    .version(BuildConfig.VERSION_CODE)
+                                val connection = object : ServiceConnection {
+                                    override fun onServiceConnected(
+                                        name: ComponentName,
+                                        service: IBinder
+                                    ) {
+                                        val serviceInterface =
+                                            IRemoteFileService.Stub.asInterface(service)
+                                        continuation.resume(serviceInterface)
+                                    }
 
-                                override fun onServiceDisconnected(name: ComponentName) {
-                                    if (continuation.isActive) {
-                                        continuation.resumeWithException(
-                                            RemoteFileSystemException("Sui service disconnected")
-                                        )
+                                    override fun onServiceDisconnected(name: ComponentName) {
+                                        if (continuation.isActive) {
+                                            continuation.resumeWithException(
+                                                RemoteFileSystemException(
+                                                    "Sui service disconnected"
+                                                )
+                                            )
+                                        }
+                                    }
+
+                                    override fun onBindingDied(name: ComponentName) {
+                                        if (continuation.isActive) {
+                                            continuation.resumeWithException(
+                                                RemoteFileSystemException("Sui binding died")
+                                            )
+                                        }
+                                    }
+
+                                    override fun onNullBinding(name: ComponentName) {
+                                        if (continuation.isActive) {
+                                            continuation.resumeWithException(
+                                                RemoteFileSystemException("Sui binding is null")
+                                            )
+                                        }
                                     }
                                 }
-
-                                override fun onBindingDied(name: ComponentName) {
-                                    if (continuation.isActive) {
-                                        continuation.resumeWithException(
-                                            RemoteFileSystemException("Sui binding died")
-                                        )
-                                    }
+                                Shizuku.bindUserService(serviceArgs, connection)
+                                continuation.invokeOnCancellation {
+                                    Shizuku.unbindUserService(serviceArgs, connection, true)
                                 }
-
-                                override fun onNullBinding(name: ComponentName) {
-                                    if (continuation.isActive) {
-                                        continuation.resumeWithException(
-                                            RemoteFileSystemException("Sui binding is null")
-                                        )
-                                    }
-                                }
-                            }
-                            Shizuku.bindUserService(serviceArgs, connection)
-                            continuation.invokeOnCancellation {
-                                Shizuku.unbindUserService(serviceArgs, connection, true)
                             }
                         }
+                    } catch (e: TimeoutCancellationException) {
+                        throw RemoteFileSystemException(e)
                     }
-                } catch (e: TimeoutCancellationException) {
-                    throw RemoteFileSystemException(e)
                 }
+            } catch (e: InterruptedException) {
+                throw RemoteFileSystemException(e)
             }
         }
     }
