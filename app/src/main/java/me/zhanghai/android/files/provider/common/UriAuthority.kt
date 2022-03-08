239a204d32af2baccfd4851bb59ef404b2caee60
Hai Zhang <dreaming.in.code.zh@gmail.com>
2022-03-08 03:25:27 -0800

    [Fix] Fix crash when editing SFTP/SMB host.

--- app/src/main/java/me/zhanghai/android/files/provider/common/UriAuthority.kt
+++ app/src/main/java/me/zhanghai/android/files/provider/common/UriAuthority.kt
@@ -8,16 +8,27 @@ data class UriAuthority(
     val host: String,
     val port: Int?
 ) {
-    fun encode(): String = toUri().rawAuthority ?: ""
-
-    override fun toString(): String = toUri().authority ?: ""
-
-    private fun toUri(): URI {
-        return try {
+    fun encode(): String {
+        val uri = try {
             // HACK: An empty host/authority requires a path, so use "/" as path here.
             URI(null, userInfo, host, port ?: -1, "/", null, null)
         } catch (e: URISyntaxException) {
             throw IllegalArgumentException(e)
+        }
+        // URI.getRawAuthority() returns null when authority is empty.
+        return uri.rawAuthority ?: ""
+    }
+
+    // toString() is called by UI when the URI may not be valid, so build the string manually.
+    override fun toString(): String = buildString {
+        if (userInfo != null) {
+            append(userInfo)
+            append('@')
+        }
+        append(host)
+        if (port != null) {
+            append(':')
+            append(port.toString())
         }
     }
 
