0581bad2e12d7c4602bd565250bead90673fc960
Hai Zhang <dreaming.in.code.zh@gmail.com>
2022-04-17 13:35:25 -0700

    [Refactor] Reformat ThemedFastScroller code.

--- app/src/main/java/me/zhanghai/android/files/ui/ThemedFastScroller.kt
+++ app/src/main/java/me/zhanghai/android/files/ui/ThemedFastScroller.kt
@@ -10,8 +10,5 @@ import me.zhanghai.android.fastscroll.FastScroller
 import me.zhanghai.android.fastscroll.FastScrollerBuilder
 
 object ThemedFastScroller {
-    fun create(view: ViewGroup): FastScroller
-        = FastScrollerBuilder(view)
-            .useMd2Style()
-            .build()
+    fun create(view: ViewGroup): FastScroller = FastScrollerBuilder(view).useMd2Style().build()
 }
