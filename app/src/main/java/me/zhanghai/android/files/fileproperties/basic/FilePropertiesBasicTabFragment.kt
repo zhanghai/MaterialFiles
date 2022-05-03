fbbd4f552767a0e1f28a9007a563e4d5b389d8f4
Hai Zhang <dreaming.in.code.zh@gmail.com>
2022-05-02 18:07:50 -0700

    [Fix] Fix crash when viewing properties of entry of non-linux archive file.
    Fixes: #773

--- app/src/main/java/me/zhanghai/android/files/fileproperties/basic/FilePropertiesBasicTabFragment.kt
+++ app/src/main/java/me/zhanghai/android/files/fileproperties/basic/FilePropertiesBasicTabFragment.kt
@@ -25,6 +25,7 @@ import me.zhanghai.android.files.file.fileSize
 import me.zhanghai.android.files.file.formatLong
 import me.zhanghai.android.files.filelist.getMimeTypeName
 import me.zhanghai.android.files.filelist.name
+import me.zhanghai.android.files.filelist.toUserFriendlyString
 import me.zhanghai.android.files.fileproperties.FilePropertiesFileViewModel
 import me.zhanghai.android.files.fileproperties.FilePropertiesTabFragment
 import me.zhanghai.android.files.provider.archive.ArchiveFileAttributes
@@ -60,7 +61,9 @@ class FilePropertiesBasicTabFragment : FilePropertiesTabFragment() {
             val path = file.path
             if (path.isArchivePath) {
                 val archiveFile = path.archiveFile
-                addItemView(R.string.file_properties_basic_archive_file, archiveFile.toFile().path)
+                addItemView(
+                    R.string.file_properties_basic_archive_file, archiveFile.toUserFriendlyString()
+                )
                 val attributes = file.attributes as ArchiveFileAttributes
                 addItemView(R.string.file_properties_basic_archive_entry, attributes.entryName())
             } else {
