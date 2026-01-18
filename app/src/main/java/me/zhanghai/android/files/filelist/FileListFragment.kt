44ff2526715bb3b9e43f8ea28e3a45752e3ff155
VBansal99 <varunbansaldcsamca@gmail.com>
2026-01-18 16:15:46 +0530

    Clearing search text does not refresh the file list

--- app/src/main/java/me/zhanghai/android/files/filelist/FileListFragment.kt
+++ app/src/main/java/me/zhanghai/android/files/filelist/FileListFragment.kt
@@ -420,8 +420,12 @@ class FileListFragment : Fragment(), BreadcrumbLayout.Listener, FileListAdapter.
                     return false
                 }
                 viewModel.searchViewQuery = query
-                debouncedSearchRunnable()
-                return false
+                if (query.isBlank()) {
+                    viewModel.stopSearching()
+                } else {
+                    debouncedSearchRunnable()
+                }
+                return true
             }
         })
         if (viewModel.isSearchViewExpanded) {
