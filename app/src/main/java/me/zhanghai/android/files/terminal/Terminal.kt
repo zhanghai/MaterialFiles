582f6739db25df4ca701f1f71ba22fce33bc1347
josef.lindbom@gmail.com <josef.lindbom@gmail.com>
2026-07-06 09:10:48 +0300

    fix: Terminal.open now uses Uri.fromFile() with text/plain MIME type

--- app/src/main/java/me/zhanghai/android/files/terminal/Terminal.kt
+++ app/src/main/java/me/zhanghai/android/files/terminal/Terminal.kt
@@ -11,18 +11,21 @@ import android.content.Intent
 import android.net.Uri
 import me.zhanghai.android.files.app.packageManager
 import me.zhanghai.android.files.util.startActivitySafe
+import java.io.File
 
 object Terminal {
     fun open(path: String, context: Context) {
+        val uri = Uri.fromFile(File(path))
         val componentName =
-            packageManager.queryIntentActivities(Intent(Intent.ACTION_SEND).setType("*/*"), 0)
+            packageManager.queryIntentActivities(Intent(Intent.ACTION_SEND).setType("text/plain"), 0)
                 .firstOrNull { it.activityInfo.name.endsWith(".TermHere") }?.activityInfo
                 ?.let { ComponentName(it.packageName, it.name) }
                 ?: ComponentName("jackpal.androidterm", "jackpal.androidterm.TermHere")
         val intent = Intent()
             .setComponent(componentName)
             .setAction(Intent.ACTION_SEND)
-            .putExtra(Intent.EXTRA_STREAM, Uri.parse(path))
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_STREAM, uri)
         context.startActivitySafe(intent)
     }
 }
